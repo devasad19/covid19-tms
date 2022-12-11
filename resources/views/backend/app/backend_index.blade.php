ceb0013b9c25ed6951fb5b3961800deeb472cef1
asadgithubuser <mdasad.cse@gmail.com>
2022-12-11 17:47:41 +0600

    from office init project

---
+++ resources/views/backend/app/backend_index.blade.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Covid-19 Testing Management System | New User Testing</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('css/fontawesome/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+<style type="text/css">
+label{
+    font-size:16px;
+    font-weight:bold;
+    color:#000;
+}
+
+</style>
+  <script>
+function mobileAvailability() {
+    $("#loaderIcon").show();
+    jQuery.ajax({
+    url: "check_availability.php",
+    data:'mobnumber='+$("#mobilenumber").val(),
+    type: "POST",
+    success:function(data){
+    $("#mobile-availability-status").html(data);
+    $("#loaderIcon").hide();
+    },
+    error:function (){}
+    });
+}
+</script>
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+@include('backend.includes.sidebar')
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+          @include('backend.includes.topbar')
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                 @yield('content')
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+           @include('backend.includes.footer')
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+ 
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('js/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+      
+@yield('scripts')
+
+
+</body>
+
+</html>
---
+++ resources/views/backend/vaccine_reg.blade.php
@@ -0,0 +1,101 @@
+@extends('backend.app.backend_index')
+
+@section('content')
+
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Registration Process</h1>
+                    <form name="newtesting" method="post">
+                      <div class="row">
+
+                        <div class="col-lg-6">
+
+                           <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Registration By</h6>
+                                </div>
+                                <div class="card-body">
+                             <div class="form-group">
+                                  <select class="form-control" id="regi_type_select" name="testtype" required="true">
+                                <option value="">Select</option> 
+                                <option value="nid">National ID</option>  
+                                <option value="bc">Birth Certificate</option> 
+                                  </select>
+                            </div>
+ 
+
+                                </div>
+                            </div>
+                       
+
+                        </div>
+
+
+                        <div class="col-lg-6">
+
+                            <!-- Basic Card Example -->
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Verify Information</h6>
+                                </div>
+                                <div class="card-body">
+                                        <div class="form-group" id="nid_section">
+                                               <label>National ID</label>
+                                            <input type="text" class="form-control" id="" name="nid" placeholder="enter NID">
+                                        </div>
+                                        <div class="form-group" id="bc_section">
+                                               <label>Birth Certificate</label>
+                                            <input type="text" class="form-control" id="" name="bc" placeholder="enter birth certificate"  >
+                                        </div>
+                                        <div class="form-group">
+                                             <label>Date Of Birth</label>
+                                            <input type="date" class="form-control" id="dob" name="dob" required="true">
+                                        </div>
+                                         
+                                     <div class="form-group">
+                                        <input type="submit" class="btn btn-primary btn-user" value="Verify" name="submit">                           
+                                 </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+
+
+                    </div>
+</form>
+
+                </div>
+@endsection
+
+
+@section('scripts')
+    <script>
+        $(document).ready(function(){
+            $('#bc_section').css('display', 'none')
+
+            $('#regi_type_select').on('change', function(){
+                var vall = $(this).val()
+
+                if(vall == 'bc'){
+
+                    $('#nid_section').css('display', 'none')
+                    $('#bc_section').css('display', 'block')
+                }else{
+
+                    $('#bc_section').css('display', 'none')
+                    $('#nid_section').css('display', 'block')
+                }
+            })
+        })
+    </script>
+
+@endsection
+
+
+
+
+
+
